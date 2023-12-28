0d045e1c0517d56fdf741934b11b8c53c9dc3b33
giuliamari04 <giuliamari04@gmail.com>
2023-12-28 12:13:47 +0100

    seeder e model account

---
+++ database/seeders/AccountsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Account;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AccountsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $accounts = file_get_contents(__DIR__.'/users.json');
+        $accounts = json_decode($accounts, true);
+        foreach($accounts as $account){
+            $newaccount = new Account();
+            $newaccount-> name = $account['name'];
+            $newaccount->surname = $account['surname'];
+            $newaccount->email = $accounts['email'];
+            $newaccount->password = $account['password'];
+            $newaccount->save();
+        }
+    }
+}
