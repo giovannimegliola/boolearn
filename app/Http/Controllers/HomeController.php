4ccd5e8f384247a417297243ef1dfb3e3dc20370
Eecodev <mnasser.bbk@gmail.com>
2024-01-02 18:49:44 +0100

    footer scaffolding

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,36 @@ class HomeController extends Controller
 {
     public function index() {
         $courses = Course::where('id','<', 5);
-        return view('home', compact('courses'));
+        $topFooterContent =
+        [
+            'Browse Courses' => [
+                'Learn Digital Marketing', 'Learn Advanced Photography', 'Learn Spanish', 'Learn Time Management', 'Learn 3D Graphics', 'Learn Android Development','Learn Advanced Data Science', 'Learn Machine Learning Fundamentals'
+            ],
+            'Start a New Career' => [
+                'Learn Digital Marketing', 'Learn Advanced Photography', 'Learn Spanish', 'Learn Time Management', 'Learn 3D Graphics', 'Learn Android Development','Learn Advanced Data Science', 'Learn Machine Learning Fundamentals'
+            ],
+            'Step-by-Step Guides' => [
+                'Learn Digital Marketing', 'Learn Advanced Photography', 'Learn Spanish', 'Learn Time Management', 'Learn 3D Graphics', 'Learn Android Development','Learn Advanced Data Science', 'Learn Machine Learning Fundamentals'
+            ],
+            'Complete Your Bachelor\'s Online' => [
+                'Learn Digital Marketing', 'Learn Advanced Photography', 'Learn Spanish', 'Learn Time Management', 'Learn 3D Graphics', 'Learn Android Development','Learn Advanced Data Science', 'Learn Machine Learning Fundamentals'
+            ],
+            'Earn Your Online Graduate Degree' => [
+                'Learn Digital Marketing', 'Learn Advanced Photography', 'Learn Spanish', 'Learn Time Management', 'Learn 3D Graphics', 'Learn Android Development','Learn Advanced Data Science', 'Learn Machine Learning Fundamentals'
+            ]
+        ];
+        $bottomFooterContent =
+        [
+            'Boolearn' => [
+                'Boot Camps', 'About', 'Boolearn for Business', 'Affiliates', 'Open Boolearn', 'Careers','News',
+            ],
+            'Connect' => [
+                'Idea Hub', 'Contact Us', 'Help Center', 'Security', 'Media Kit',
+            ],
+            'Legal' => [
+                'Terms of Service', 'Privacy Policy', 'Cookie Policy', 'Accessibility Policy', 'Trademark Policy',
+            ]
+        ];
+        return view('home', compact('courses', 'topFooterContent', 'bottomFooterContent'));
     }
 }
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,64 @@
-<div>footer</div>
+<footer>
+    <div class="container d-flex justify-content-between w-100 pb-4 pt-4">
+        <a href="#"><img src="/public/img/logo.png" alt="boolearn footer logo"></a>
+        <div>
+            <ul class="list-unstyled d-flex">
+                <li class="mx-2"><a href="#" class="social-links"><i class="fa-brands fa-facebook"></i></a></li>
+                <li class="mx-2"><a href="#" class="social-links"><i class="fa-brands fa-twitter"></i></a></li>
+                <li class="mx-2"><a href="#" class="social-links"><i class="fa-brands fa-linkedin"></i></a></li>
+                <li class="mx-2"><a href="#" class="social-links"><i class="fa-brands fa-reddit-alien"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row row-cols-3" id="top-foot">
+            @foreach($topFooterContent as $sectionTitle => $items)
+                <div class="col pb-5">
+                    <h5 class="text-uppercase font-weight-lighter pb-3">{{$sectionTitle}}</h5>
+                    <ul class="p-0 list-unstyled">
+                        @foreach ($items as $item)
+                            <li class="pb-2"><a href="#" class="footer-links">{{$item}}</a></li>
+                        @endforeach
+                    </ul>
+                    <a href="#">see more</a>
+                </div>
+            @endforeach
+        </div>
+      </div>
+    <div class="container d-flex justify-content-between" id="bottom-foot">
+        <div class="d-flex">
+            @foreach($bottomFooterContent as $sectionTitle => $items)
+                <div class="bottom-list">
+                    <h5 class="list-title"><strong>{{$sectionTitle}}</strong></h5>
+                    <ul class="p-0 list-unstyled">
+                        @foreach ($items as $item)
+                            <li class="pb-2"><a href="#" class="footer-links">{{$item}}</a></li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endforeach
+        </div>
+        <div class="pr-3">
+            <h5 class="pb-2"><strong>Choose Language</strong></h5>
+            <div class="d-flex">
+                <select class="form-select mx-2" aria-label="Default select example">
+                    <option selected>English</option>
+                    <option value="1">Italian</option>
+                </select>
+                <button class="btn border">Apply</button>
+            </div>
+        </div>
+    </div>
+    <div class=" container d-flex justify-content-between flex-wrap-reverse body-small pt-3">
+        <p>© 2024 Boolearn. All rights reserved.</p>
+        <div class="app-download-links large-column d-flex flex-row justify-content-between p-0 mb-5">
+            <a href="#">
+                <img src="/public/img/app-store-btn.png" alt="App Store-button">
+            </a>
+            <a href="#">
+                <img src="/public/img/google-play-btn.png" alt="Google Play-button">
+            </a>
+        </div>
+    </div>
+</footer>
+
