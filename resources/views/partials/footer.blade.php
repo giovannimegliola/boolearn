892a213ce684ebc64500341c3980e3b00c7ddab0
Eecodev <mnasser.bbk@gmail.com>
2024-01-04 12:56:56 +0100

    footer update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
-    <div class="container d-flex justify-content-between w-100 pb-4 pt-4">
+    <div class="container d-flex justify-content-between align-items-center w-100 pb-4 pt-4">
         <a href="#"><img src="/img/logo.png" class="logo-footer" alt="boolearn footer logo"></a>
         <div>
             <ul class="list-unstyled d-flex">
@@ -14,13 +14,13 @@
         <div class="row row-cols-3" id="top-foot">
             @foreach($topFooterContent as $sectionTitle => $items)
                 <div class="col pb-5">
-                    <h5 class="text-uppercase font-weight-lighter pb-3">{{$sectionTitle}}</h5>
+                    <h5 class="list-title text-uppercase font-weight-lighter pb-3">{{$sectionTitle}}</h5>
                     <ul class="p-0 list-unstyled">
                         @foreach ($items as $item)
                             <li class="pb-2"><a href="#" class="footer-links">{{$item}}</a></li>
                         @endforeach
                     </ul>
-                    <a href="#">see more</a>
+                    <a href="#" class="see-more">see more</a>
                 </div>
             @endforeach
         </div>
@@ -39,19 +39,19 @@
             @endforeach
         </div>
         <div class="pr-3">
-            <h5 class="pb-2"><strong>Choose Language</strong></h5>
+            <h5 class="pb-2 select-lang"><strong>Choose Language</strong></h5>
             <div class="d-flex">
                 <select class="form-select mx-2" aria-label="Default select example">
                     <option selected>English</option>
                     <option value="1">Italian</option>
                 </select>
-                <button class="btn border">Apply</button>
+                <button class="btn border select-lang">Apply</button>
             </div>
         </div>
     </div>
     <div class=" container d-flex justify-content-between flex-wrap-reverse body-small pt-3">
-        <p>© 2024 Boolearn. All rights reserved.</p>
-        <div class="app-download-links large-column d-flex flex-row justify-content-between p-0 mb-5">
+        <p class="copyright">© 2024 Boolearn. All rights reserved.<br>进入网络空间</p>
+        <div class="app-download-links large-column d-flex justify-content-between p-0 mb-5">
             <a href="#">
                 <img src="/img/app-store-btn.png" class="app-btn" alt="App Store-button">
             </a>
