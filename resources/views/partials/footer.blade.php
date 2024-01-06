b3040de4f8d66ae69fb4135f5332df4c48354c1b
Eecodev <mnasser.bbk@gmail.com>
2024-01-06 13:01:52 +0100

    website' s layout update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,10 +3,10 @@
         <a href="#"><img src="{{Vite::asset('/resources/img/our_logo.png')}}" class="logo-footer" alt="boolearn-footer-logo"></a>
         <div>
             <ul class="list-unstyled d-flex">
-                <li class="mx-2"><a href="#" class="social-links"><i class="fa-brands fa-facebook"></i></a></li>
-                <li class="mx-2"><a href="#" class="social-links"><i class="fa-brands fa-twitter"></i></a></li>
-                <li class="mx-2"><a href="#" class="social-links"><i class="fa-brands fa-linkedin"></i></a></li>
-                <li class="mx-2"><a href="#" class="social-links"><i class="fa-brands fa-reddit-alien"></i></a></li>
+                <li class="mx-2"><a href="https://www.facebook.com/?locale=it_IT" target="_blank" class="social-links"><i class="fa-brands fa-facebook"></i></a></li>
+                <li class="mx-2"><a href="https://twitter.com/?lang=it" target="_blank" class="social-links"><i class="fa-brands fa-twitter"></i></a></li>
+                <li class="mx-2"><a href="https://it.linkedin.com/?src=go-pa&trk=sem-ga_campid.20677489327_asid.153452322614_crid.677545198980_kw.linkedin_d.c_tid.kwd-148086543_n.g_mt.e_geo.1008089&mcid=7120409760495009793&cid=&gclid=Cj0KCQiAkeSsBhDUARIsAK3tieesIPeYjr2BXvqWmoaQRjgCyUw8kSmNsRPxLwDcms5lF4q2Ua3rOmgaArXiEALw_wcB&gclsrc=aw.ds" target="_blank" class="social-links"><i class="fa-brands fa-linkedin"></i></a></li>
+                <li class="mx-2"><a href="https://www.reddit.com/?rdt=42431" target="_blank" class="social-links"><i class="fa-brands fa-reddit-alien"></i></a></li>
             </ul>
         </div>
     </div>
@@ -52,10 +52,10 @@
     <div class=" container d-flex justify-content-between flex-wrap-reverse body-small pt-3">
         <p class="copyright">© 2024 Boolearn. All rights reserved.<br>进入网络空间</p>
         <div class="app-download-links large-column d-flex justify-content-between p-0 mb-5">
-            <a href="#">
+            <a href="https://www.apple.com/app-store/" target="_blank">
                 <img src="{{Vite::asset('/resources/img/app-store-btn.png')}}" class="app-btn" alt="App Store-button">
             </a>
-            <a href="#">
+            <a href="https://play.google.com/store/games?hl=en&gl=US&pli=1" target="_blank">
                 <img src="{{Vite::asset('/resources/img/google-play-btn.png')}}" class="app-btn" alt="Google Play-button">
             </a>
         </div>
